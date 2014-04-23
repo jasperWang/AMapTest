b897df9bd7599b6b2d72ec7966c0394d2cc5144f
Jasper <wangyin@boqii-dwg1063>
2014-04-23 17:32:27 +0800

    17:32

--- src/cn/boqii/app/ui/ShareActivity.java
+++ src/cn/boqii/app/ui/ShareActivity.java
@@ -6,7 +6,6 @@ import cn.sharesdk.framework.Platform;
 import cn.sharesdk.framework.PlatformActionListener;
 import cn.sharesdk.framework.ShareSDK;
 import cn.sharesdk.framework.utils.UIHandler;
-import cn.sharesdk.onekeyshare.EditPage;
 import cn.sharesdk.onekeyshare.OnekeyShare;
 import android.app.Activity;
 import android.content.Intent;
@@ -25,6 +24,7 @@ public class ShareActivity extends Activity implements Callback {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		ShareSDK.initSDK(this);
+		 
 		setContentView(R.layout.share_layout);
 	}
 
@@ -34,29 +34,28 @@ public class ShareActivity extends Activity implements Callback {
 	public void onAction(View v) {
 		switch (v.getId()) {
 		case R.id.shareViewBtn:
-
 			OnekeyShare oks = new OnekeyShare();
 			// 分享时 Notification 的图标和文字
 			oks.setNotification(R.drawable.ic_launcher,
 					this.getString(R.string.app_name));
 
 			oks.setAddress("15803017438");// 仅在信息和邮箱时使用
-			oks.setTitle("分享功能测试");// 标题
+			oks.setTitle("分享测试");// 标题
 			oks.setTitleUrl("http://bbs.boqii.com/content/viewthread-3130655.html");// 标题超链接
-			oks.setText("这是为了实现分享功能的测试Demo");// 内容
+			oks.setText("波奇宠物生活馆：http://www.boqii.com");// 内容
 			// oks.setImagePath(TEST_IMAGE);// 本地图片
 			// oks.setImageUrl("http://bbs.boqii.com/content/viewthread-3130655.html");//
 			// 图片指向的链接
-			oks.setComment("( ^_^ )不错嘛");// 分享的评论，仅在人人网和 QQ 空间使用
-			oks.setSite("波奇生活馆");// site 是分享应用的名称，仅在 QQ 空间使用
+			oks.setComment("还能不能让宠物自由的玩耍？");// 分享的评论，仅在人人网和 QQ 空间使用
+			oks.setSite("http://www.boqii.com");// site 是分享应用的名称，仅在 QQ 空间使用
 			// siteUrl 是分享此内容的网站地址，仅在 QQ 空间使用
 			oks.setSiteUrl("http://bbs.boqii.com/content/viewthread-3130655.html");
 			// latitude 是维度数据，仅在新浪微博、腾讯微博和 Foursquare 使用
 			oks.setLatitude(31.238068f);
 			// longitude 是经度数据，仅在新浪微博、腾讯微博和 Foursquare 使用
 			oks.setLongitude(121.501654f);
-			oks.setImagePath(TEST_IMAGE);
-			oks.setImageUrl("www.boqii.com");
+			//oks.setImagePath(TEST_IMAGE);
+			//oks.setImageUrl("http://www.boqii.com");
 			// 是否直接分享（true 则直接分享）
 			// oks.setSilent(true);
 
@@ -111,6 +110,7 @@ public class ShareActivity extends Activity implements Callback {
 		case -1:
 			Toast.makeText(ShareActivity.this, "分享失败，原因：" + msg.obj,
 					Toast.LENGTH_LONG).show();
+			System.out.println("分享失败，原因：" + msg.obj);
 			break;
 		case 1:
 			Toast.makeText(ShareActivity.this, "分享成功", Toast.LENGTH_SHORT)
